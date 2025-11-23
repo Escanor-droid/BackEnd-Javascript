8c96464b8fc620a3c93e45944b430720919b05eb
Escanor-droid <mohitnegi560@gmail.com>
2025-11-23 15:48:36 +0530

    Util code

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
 });
 
 
-connectDB();
+connectDB().then(() => {
+    app.on("error" , ((err) => {
+        console.log("Error starting the server", err);
+        throw err;
+    }))
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+
+}).catch((err) => {
+    console.log("MONGODB Connection failed !!!", err);
+})
 
 
 
